825dcab3325bf5da34d4975b3f9b142d14e4100d
IssamLaribi <laribiissam8@gmail.com>
2026-03-31 02:39:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""Create python env"""
+# conda create -n intro_to_ai_project python=3.12
+"""Activate it"""
+# conda activate intro_to_ai_project
+# conda install ipykernel 
+# python -m ipykernel install --user --name intro_to_ai_project --display-name "Python (intro_to_ai_project)"
+# The modules will be installed later later
